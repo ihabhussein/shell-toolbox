71e31871651cc0d0c90fd9e178c65d10e969d9af
Ihab Hussein <ihab@ihabhussein.com>
2019-01-31 12:35:57 +0200

    Add more scripts

--- man.sh
+++ man.sh
@@ -7,7 +7,7 @@ for f in $(grep -l __DATA__ "$here"/bin/*); do
     pod2man "$f" "$here/man/$g.1"
 done
 
-for f in $(grep -l '<< *HELP_TEXT' "$here"/bin/* "$here"/lib/*); do
+for f in $(grep -l '<<HELP_TEXT' "$here"/bin/* "$here"/lib/*); do
     g=$(basename "$f")
     sed "1,/<< *HELP_TEXT/d; s/__CMD__/$g/g;" < "$f" > "$here/man/$g.1"
 done
